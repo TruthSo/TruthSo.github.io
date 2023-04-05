d55d57d422e69eeaedfbf22970cf3a1a4d3b067f
WenJun <wenjunn11@gmail.com>
2023-04-05 14:18:14 +0800

    fixed copy function

--- CopyBox/main.js
+++ CopyBox/main.js
@@ -16,26 +16,26 @@ copyButtons.forEach(function(button, index) {
   button.addEventListener("click", function() {
     // Copy the content of the corresponding textarea to the clipboard
     const textareaEl = button.previousElementSibling;
-    const range = document.createRange();
-    range.selectNode(textareaEl);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand("copy");
 
-    // Log the copied text to the console
-    console.log(`Copied text from Box ${index + 1}: ${textareaEl.value}`);
+    if (textareaEl && textareaEl.tagName.toLowerCase() === "textarea") {
+      textareaEl.select();
+      document.execCommand("copy");
+    
+      // Log the copied text to the console
+      console.log(`Copied text from Box ${index + 1}: ${textareaEl.value}`);
+    
+      // Show a notification message
+      const notification = document.createElement("div");
+      notification.classList.add("show");
+      notification.classList.add("notification");
+      notification.textContent = `From Box ${index + 1} content has copied!`;
+      document.body.appendChild(notification);
 
-    // Show a notification message
-    const notification = document.createElement("div");
-    notification.classList.add("show");
-    notification.classList.add("notification");
-    notification.textContent = `From Box ${index + 1} content has copied!`;
-    document.body.appendChild(notification);
-
-    // Remove the notification message after 2 seconds
-    setTimeout(function() {
-      notification.remove();
-      notification.classList.remove("show");
-    }, 5000);
-  });
+       // Remove the notification message after 2 seconds
+      setTimeout(function() {
+        notification.remove();
+        notification.classList.remove("show");
+      }, 5000);
+    }
+  });  
 });
