e904cb196e76cd34cd150a9783849011b77b46dd
WenJun <wenjunn11@gmail.com>
2023-04-05 15:58:37 +0800

    add rainbow line and customizable label description

--- CopyBox/main.js
+++ CopyBox/main.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', function() {
-    const boxLabels = ['test1', 'test2', 'test3', 'test4', 'test5'];
-    const boxes = document.querySelectorAll('.box');
+    // const boxLabels = ['test1', 'test2', 'test3', 'test4', 'test5'];
+    // const boxes = document.querySelectorAll('.box');
   
-    for (let i = 0; i < boxes.length; i++) {
-      const label = boxes[i].querySelector('label');
-      label.textContent = `Box ${i + 1}: ${boxLabels[i]}`;
-    }
+    // for (let i = 0; i < boxes.length; i++) {
+    //   const label = boxes[i].querySelector('label');
+    //   label.textContent = `Box ${i + 1}: ${boxLabels[i]}`;
+    // }
   });
   
 
@@ -39,3 +39,31 @@ copyButtons.forEach(function(button, index) {
     }
   });  
 });
+
+const table = document.querySelector('table');
+const tbody = table.querySelector('tbody');
+
+for (let i = 1; i <= 5; i++) {
+  const row = document.createElement('tr');
+  row.classList.add('data-row');
+
+  // Add first column to row
+  const labelCell = document.createElement('td');
+  labelCell.textContent = `Box ${i+1}`;
+  row.appendChild(labelCell);
+
+  // Add empty cells to row
+  const valueCell = document.createElement('td');
+  valueCell.setAttribute('contenteditable', 'true');
+  row.appendChild(valueCell);
+
+  valueCell.addEventListener('input', function() {
+    const boxLabel = `Box ${i}: ${valueCell.textContent}`;
+    const box = document.querySelector(`.box:nth-of-type(${i}) label`);
+    box.textContent = boxLabel;
+  });
+
+  
+  tbody.appendChild(row);
+}
+
